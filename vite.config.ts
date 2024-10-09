41aad3b60d11f6bcb7ae92d6e89e33905a6eb276
Rafa Borges <rafael.borges@glovoapp.com>
2024-10-09 20:54:47 +0200

    Fixed the relative issue for the demo page

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {version as package_json_version} from "./package.json";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   define: {
     __BOOKMARKS_CARDS_VERSION__: JSON.stringify(package_json_version),
     __BOOKMARKS_CARDS_DATE__: JSON.stringify(new Date().toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' }))
